6857bfe5141d4cf1341246c1757d76cd4b5d2ea9
Sulman <shahsuliman@gmail.com>
2021-05-14 06:20:09 +0500

    Added the first screen and the images

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 // Uncomment lines 7 and 10 to view the visual layout at runtime.
 // import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
@@ -10,111 +11,168 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
-        ],
-      ),
-    );
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildButtonColumn(color, Icons.call, 'CALL'),
-          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-          _buildButtonColumn(color, Icons.share, 'SHARE'),
-        ],
-      ),
-    );
-
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
     return MaterialApp(
-      title: 'Flutter layout demo',
+      home: MyHome()
+    );
+  }
+}
+
+class Todo {
+  final String title;
+  final String description;
+
+  Todo(this.title, this.description);
+}
+
+
+class MyHome extends StatelessWidget {
+  void _onTileClicked(){
+  print("tapped on container");
+  }
+
+   @override
+  Widget build(BuildContext context) {
+    final title = 'Blood Donation';
+    return MaterialApp(
+      title: title,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter layout demo'),
+          centerTitle: true,
+          title: Text(title),
         ),
-        body: ListView(
-          children: [
-            Image.asset(
-              'images/lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection,
-          ],
-        ),
+        backgroundColor: Colors.white,
+        body: GridView.count(
+         
+  primary: false,
+  padding: const EdgeInsets.all(20),
+  crossAxisSpacing: 10,
+  mainAxisSpacing: 10,
+  crossAxisCount: 2,
+  shrinkWrap: true,
+      scrollDirection: Axis.vertical,
+      physics: NeverScrollableScrollPhysics(),
+  children: <Widget>[
+    InkWell (
+      customBorder: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(20),
+),
+  onTap: () {
+    debugPrint('onTap clicked');
+    
+            Navigator.push(context,MaterialPageRoute(builder: (context)=> SecondRoute()),
+            );
+  }, // handle your image tap here
+  child: Image.asset(
+    'images/searchblooddonor@2x.png',
+    fit: BoxFit.cover, // this is the solution for border
+    width: 110.0,
+    height: 110.0,
+  ),
+),
+    InkWell(
+      customBorder: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(20),),
+  onTap: () {}, // handle your image tap here
+  child: Image.asset(
+    'images/myrequest.png',
+    fit: BoxFit.cover, // this is the solution for border
+    width: 110.0,
+    height: 110.0,
+  ),
+),
+        InkWell(
+          customBorder: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(20),),
+  onTap: () {}, // handle your image tap here
+  child: Image.asset(
+    'images/thalassemia@2x.png',
+    fit: BoxFit.cover, // this is the solution for border
+    width: 110.0,
+    height: 110.0,
+    
+  ),
+  
+),
+        InkWell(
+          customBorder: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(20),),
+  onTap: () {}, // handle your image tap here
+  child: Image.asset(
+    'images/bloodrequest.png',
+    fit: BoxFit.cover, // this is the solution for border
+    width: 110.0,
+    height: 110.0,
+  ),
+),
+  ],
+),
       ),
     );
   }
+}
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
+ Widget _gridView() {
+    return GridView.count(
+  primary: false,
+  padding: const EdgeInsets.all(20),
+  crossAxisSpacing: 10,
+  mainAxisSpacing: 10,
+  crossAxisCount: 2,
+  children: <Widget>[
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text("He'd have you all unravel at the"),
+      color: Colors.teal[100],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Heed not the rabble'),
+      color: Colors.teal[200],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Sound of screams but the'),
+      color: Colors.teal[300],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Who scream'),
+      color: Colors.teal[400],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution is coming...'),
+      color: Colors.teal[500],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution, they...'),
+      color: Colors.teal[600],
+    ),
+  ],
+);
   }
+
+
+ class SecondRoute extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+	return Scaffold(
+	appBar: AppBar(
+		title:Text("ListView.builder")
+	),
+	body: ListView.builder(
+		itemCount: 5,
+		itemBuilder: (BuildContext context,int index){
+		return ListTile(
+			leading: Icon(Icons.list),
+			trailing: Text("GFG",
+						style: TextStyle(
+							color: Colors.green,fontSize: 15),),
+			title:Text("List item $index")
+			);
+		}
+		),
+	);
+}
 }
